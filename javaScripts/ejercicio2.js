0b585c1fd07730a2b8e4c6233b8930ab220e65a0
javier471 <javiersmr471@gmail.com>
2022-09-22 12:33:04 +0200

    Añadido ejercicio2

---
+++ javaScripts/ejercicio2.js
@@ -0,0 +1,3 @@
+//Ejercicio2
+addEventListener('click',function(){document.getElementById('adicional').classList.toggle('visible')});
+addEventListener('click',function(){document.getElementById('enlace').classList.toggle('oculto')});
