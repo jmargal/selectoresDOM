bb6e2e34f8d687d1961eb3a7b3566de2fdbb2f4d
javier471 <javiersmr471@gmail.com>
2022-09-25 22:05:26 +0200

    Ejercicio4

--- javaScripts/ejercicio4.js
+++ javaScripts/ejercicio4.js
@@ -1,7 +1,22 @@
 //1 - Selecciona todos los elementos de tipo h2 y pon el texto de color rojo.
 let rojos=document.querySelectorAll('h2');
 for(let i=0;i<rojos.length;i++){
-    rojos[i].setAttribute("color","red");
+    rojos[i].setAttribute("style","color:red");
 }
 
 //2 - Modifica el precio del vuelo para que ahora sea $199.99
+let precioVuelos=document.querySelectorAll('[class="details"]');
+for(let i=0;i<precioVuelos.length;i++){
+    precioVuelos[i].innerHTML="$199.99";
+}
+
+//3 - Modifica todos los títulos de las vacaciones (h2), su nuevo valor será "Sanlúcar de Barrameda".
+for(let i=0;i<rojos.length;i++){
+    rojos[i].innerHTML="Sanlúcar de Barrameda";
+}
+
+//4 - Selecciona la lista de vacaciones cuyo id es #vacations.
+console.log(document.querySelector('#vacations'));
+
+//5 - Selecciona aquellos ítems que tenga la clase .america
+console.log(document.getElementsByClassName("america"));
---
+++ javaScripts/ejercicio5.js
@@ -0,0 +1,7 @@
+//a - Selecciona todos los módulos de primero
+let modulos=document.querySelectorAll('ul li a');
+console.log(modulos);
+
+//b - Selecciona el módulo Sistemas Informáticos
+let sistemas=document.querySelector('a[href^="SI"]');
+console.log(sistemas);
